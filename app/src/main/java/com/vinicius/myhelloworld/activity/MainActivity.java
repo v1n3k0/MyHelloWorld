c072541468d406e01febb109fbc40539a8fb0cb1
Vinicius <v1n1c1u5_1@hotmail.com>
2018-04-04 21:06:41 -0300

    Logoff do usuario

--- app/src/main/java/com/vinicius/myhelloworld/activity/MainActivity.java
+++ app/src/main/java/com/vinicius/myhelloworld/activity/MainActivity.java
@@ -1,10 +1,16 @@
 package com.vinicius.myhelloworld.activity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.vinicius.myhelloworld.R;
@@ -12,28 +18,57 @@ import com.vinicius.myhelloworld.config.ConfiguracaoFireBase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button botaoSair;
     private FirebaseAuth autenticacao;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoSair = findViewById(R.id.button_sair);
+        autenticacao = ConfiguracaoFireBase.getFirebaseAutenticacao();
 
-        botaoSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Hello World");
 
-                autenticacao = ConfiguracaoFireBase.getFirebaseAutenticacao();
-                autenticacao.signOut();
+        setSupportActionBar(toolbar);
+    }
 
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-            }
-        });
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item_configuracoes:
+                return true;
+            case R.id.item_sair:
+                deslogaUsuario();
+                return true;
+            case R.id.item_pesquisa:
+                return true;
+             default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
+
+    private void deslogaUsuario(){
+
+        autenticacao = ConfiguracaoFireBase.getFirebaseAutenticacao();
+        autenticacao.signOut();
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+
 }
