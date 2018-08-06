fd8d79c75b35a3438ba63e54861687074f02e8e5
Vinicius <v1n1c1u5_1@hotmail.com>
2018-08-06 20:27:27 -0300

    Modelando classe reposta

--- app/src/main/java/com/vinicius/myhelloworld/model/Exercicio.java
+++ app/src/main/java/com/vinicius/myhelloworld/model/Exercicio.java
@@ -1,10 +1,21 @@
 package com.vinicius.myhelloworld.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Exercicio implements Serializable {
 
+    private int id;
     private String questao;
+    private ArrayList<Resposta> respostas;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getQuestao() {
         return questao;
@@ -12,5 +23,13 @@ public class Exercicio implements Serializable {
 
     public void setQuestao(String questao) {
         this.questao = questao;
+    }
+
+    public ArrayList<Resposta> getRespostas() {
+        return respostas;
+    }
+
+    public void setRespostas(ArrayList<Resposta> repostas) {
+        this.respostas = repostas;
     }
 }
