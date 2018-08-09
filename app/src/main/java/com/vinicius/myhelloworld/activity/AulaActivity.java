5f29146eee5757e9664ef42a12276888ecd5236b
Vinicius <v1n1c1u5_1@hotmail.com>
2018-08-08 21:25:02 -0300

    Correção nos numero das questoes e respostas. E nova cores

--- app/src/main/java/com/vinicius/myhelloworld/activity/AulaActivity.java
+++ app/src/main/java/com/vinicius/myhelloworld/activity/AulaActivity.java
@@ -3,9 +3,12 @@ package com.vinicius.myhelloworld.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vinicius.myhelloworld.R;
 import com.vinicius.myhelloworld.adapter.RespostaAdapter;
@@ -17,35 +20,63 @@ import java.util.ArrayList;
 
 public class AulaActivity extends AppCompatActivity {
 
-    private TextView texto;
+    private TextView questao;
+    private TextView questaoId;
     private Aula aula;
     private ArrayAdapter adapter;
     private ListView listView;
+    private ArrayList<Resposta> respostas;
+    private int numeroExercicio;
+    private int tamanhoListaExercicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aula);
 
-        texto = findViewById(R.id.textView_questao);
+        questao = findViewById(R.id.textView_questao);
+        questaoId = findViewById(R.id.textView_questao_id);
+        listView = findViewById(R.id.lv_repostas);
 
         //Recuperar dados da intent
         Intent intent = getIntent();
         if(intent != null) aula = (Aula) intent.getSerializableExtra("aula");
 
-        editarView(aula.getExercicios().get(1));
+
+        tamanhoListaExercicio = aula.getExercicios().size();
+        numeroExercicio = 0;
+        editarView(aula.getExercicios().get(numeroExercicio));
+
+        //Click da lista de respostas
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Lista de resposta
+                Resposta resposta = respostas.get(position);
+
+                numeroExercicio++;
+                if(resposta.isCerto() && numeroExercicio < tamanhoListaExercicio){
+                    editarView(aula.getExercicios().get(numeroExercicio));
+                }else{
+                    Toast.makeText(AulaActivity.this, "Resposta errada. Tente novamente", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
+    //Mostra a questão e respostas
     private void editarView(Exercicio exercicio){
 
-        texto.setText(exercicio.getQuestao());
+        respostas = exercicio.getRespostas();
 
-        ArrayList<Resposta> respostas = exercicio.getRespostas();
+        //Mostrar Questão
+        questaoId.setText(Integer.toString(exercicio.getId() + 1));
+        questao.setText(exercicio.getQuestao());
 
         //Montar listView Adapter
-        listView = findViewById(R.id.lv_repostas);
         adapter = new RespostaAdapter(this, respostas);
         listView.setAdapter(adapter);
-
     }
 }
