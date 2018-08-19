e648b23b9372e01fa92e1252920d0d1d7ceaf5c0
Vinicius <v1n1c1u5_1@hotmail.com>
2018-08-19 20:40:50 -0300

    Finalizado

--- app/src/main/java/com/vinicius/myhelloworld/activity/AulaFimActivity.java
+++ app/src/main/java/com/vinicius/myhelloworld/activity/AulaFimActivity.java
@@ -3,6 +3,8 @@ package com.vinicius.myhelloworld.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -21,8 +23,10 @@ public class AulaFimActivity extends AppCompatActivity {
     private TextView pontos;
     private TextView level;
     private TextView experiencia;
+    private Button botaoVoltar;
     private Aula aula;
     private Usuario usuario;
+    private String identificadorUsuarioLogado;
     private DatabaseReference firebase;
 
     @Override
@@ -34,6 +38,7 @@ public class AulaFimActivity extends AppCompatActivity {
         pontos = findViewById(R.id.textView_aulaFim_pontos);
         level = findViewById(R.id.textView_aulaFim_level);
         experiencia = findViewById(R.id.textView_aulaFim_experiencia);
+        botaoVoltar = findViewById(R.id.button_aulaFim_voltar);
 
         //Recuperar dados da intent
         Intent intent = getIntent();
@@ -41,7 +46,7 @@ public class AulaFimActivity extends AppCompatActivity {
 
         //Recuperar contatos do firebase
         Preferencias preferencias = new Preferencias(this);
-        String identificadorUsuarioLogado = preferencias.getIdentificador();
+        identificadorUsuarioLogado = preferencias.getIdentificador();
         firebase = ConfiguracaoFireBase.getFirebase()
                 .child("Usuario")
                 .child(identificadorUsuarioLogado);
@@ -54,6 +59,8 @@ public class AulaFimActivity extends AppCompatActivity {
                     usuario = dataSnapshot.getValue(Usuario.class);
 
                     usuario.setExperiencia(usuario.getExperiencia() + aula.getPontos());
+                    usuario.setId(identificadorUsuarioLogado);
+                    usuario.salvar();
 
                     titulo.setText(aula.getNome());
                     pontos.setText(Integer.toString(aula.getPontos()));
@@ -68,6 +75,18 @@ public class AulaFimActivity extends AppCompatActivity {
             }
         });
 
+        //Ação to botao voltar
+        botaoVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                voltarMain();
+            }
+        });
 
+    }
+
+    private void voltarMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
